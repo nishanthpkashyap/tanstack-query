0bc84165ceb59a5cef53d75497225adbf9397371
nishanth <nishanthpkashyap1503@gmail.com>
2024-06-23 20:30:32 +0530

    renamed file

--- src/services/todos/getIds.ts
+++ src/services/todos/getIds.ts
@@ -1,8 +1,5 @@
 import { Todo } from "@/types/todo";
-import { default as axiosInit } from "axios"
-
-const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
-const axios = axiosInit.create({ baseURL });
+import { axios } from "@/services";
 
 export async function getTodosId() {
     return (await axios.get<Todo[]>('todos')).data.map((todo) => todo.id);
